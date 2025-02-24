d6ca6cc0b00922e8d50b369fb22d849b867f8e71
Nicolás Corrales <ncorrales@conexia.com>
2025-02-24 11:03:32 -0300

    Se realizan correciones

--- src/app/shared/error-response.interceptor.ts
+++ src/app/shared/error-response.interceptor.ts
@@ -6,13 +6,10 @@ import { LoadingService } from './services/loading.service';
 export const ErroResponseInterceptor: HttpInterceptorFn = (req, next) => {
 
   const loadingService = inject(LoadingService);
-  // Mostrar el componente de carga al inicio de la petición
   loadingService.showLoading();
 
   return next(req).pipe(
-    // Capturar el error si ocurre
     catchError(handleErrorResponse),
-    // Finalizar la petición y ocultar el componente de carga
     finalize(() => {
       loadingService.hideLoading();
     })
